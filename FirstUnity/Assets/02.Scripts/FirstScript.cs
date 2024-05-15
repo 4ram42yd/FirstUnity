3de0e49fe783d18d0736478e674e1a29cc7128d4
4ram42yd <goydgo@gmail.com>
2024-05-15 17:28:07 +0900

    5_15

--- FirstUnity/Assets/02.Scripts/FirstScript.cs
+++ FirstUnity/Assets/02.Scripts/FirstScript.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class FirstScript : MonoBehaviour
 {
     int thisYear;
-
+    float pi;
     // Start is called before the first frame update
     void Start()
     {
         thisYear = 2024;
         print("이번 연도는 : " + thisYear + " 입니다.");
+
+        pi = 3.14f;
+        print("파이 값은 " + pi + " 입니다.");
     }
 
     // Update is called once per frame
