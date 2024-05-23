154f30f11d0ba7934f0818486e50ddd17d6aebe8
4ram42yd <goydgo@gmail.com>
2024-05-23 22:25:21 +0900

    5_23

--- FirstUnity/Assets/02.Scripts/FirstScript.cs
+++ FirstUnity/Assets/02.Scripts/FirstScript.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    int hp = 90;
-    void Start()
+    int hp = 100;
+   
+    void Update()
     {
-        hp = hp - 100;
-        if (hp < 0)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            print("게임 오버!");
-        }
+            hp = hp - 20;
+            if (hp <= 0)
+            {
+                print("게임 오버!");
+            }
+            else if(hp <= 40)
+            {
+                print("현재 체력은 : " + hp + " 입니다.");
+                print("비상!!!");
+            }
+            else if( hp <= 60)
+            {
+                print("현재 체력은 : " + hp + " 입니다.");
+                print("조심하세요!!");
+            }
+            else
+            {
+                print("현재 체력은 : " + hp + " 입니다.");
+            }
+        }    
     }
 }
